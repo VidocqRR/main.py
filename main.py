cb89960d297a3f2cc009d4487faf112c1f436a4c
Rumen Stoev <46021188+VidocqRR@users.noreply.github.com>
2018-12-24 12:42:10 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+
+                                # Bruno Mars = The Lazy Song
+
+#Song info:
+Artist = 'Bruno Mars';
+Song = 'The Lazy song';                               # The name of the song
+Genre =	'Reggae';                                     #Genre of the song
+Released = 'February 15';
+Year = 2011;                                          #The Year of issue
+Format = 'CD single, digital download';
+Lenght = 3.15;                                        #Time of the song
+Producer = 'The Smeezingtons';                         #The Producer of 'The Lazy song'
+
+
+'''
+ The Lazy Song" is a song recorded by American singer-songwriter Bruno Mars for his debut studio album Doo-Wops & Hooligans (2010). 
+ It was serviced to contemporary hit radios in the United States on February 15, 2011 as the album's third single by Atlantic and Elektra. 
+ Development of "The Lazy Song" began while Bruno Mars, Philip Lawrence and Ari Levine were hanging around the studio and didn’t feel like working. 
+ Mars wrote the song in collaboration with singer-songwriter K'naan and his production team The Smeezingtons, who also produced the track.
+ Musically, "The Lazy Song" has been described as borrowing "heavily from roots reggae", while lyrically it is an anthem to laziness.
+
+'''
+                                                                                              #The Lazy Song - Wikipedia
+print(Artist)
+print(Song)
+print(Genre)
+print(Released)
+print(Lenght)
+print(Producer)
+
+print('The song is ' , Song , 'and' , 'Artist is' , Artist, 'Year is ', Year);
